483c0a525dd3a791780a0ab4367559f3b95d32a5
Syed Sirajul Islam Anik <sirajul.islam.anik@gmail.com>
2016-11-15 23:17:56 +0600

    Time formatted to 24 hours

--- start.py
+++ start.py
@@ -9,7 +9,7 @@ from datetime import datetime
 # load the env file
 dotenv.load('./.env')
 
-# alias the dotenv.get
+# alias the dotenv.get method
 get = dotenv.get
 
 #get the time format
@@ -19,8 +19,13 @@ if format not in [12, 24]:
 	raise Exception('Time format should either be 12 or 24')
 
 def format_time(utime):
-	if "am" in utime.lower() or "pm" in utime.lower():
-		return utime[:-3]
+	if "am" in utime.lower():
+		return utime[:-2]
+	elif "pm" in utime.lower():
+		utime = utime.lower().replace('pm', '')
+		splited = utime.split(':')
+		new_time = 12 + int(splited[0])
+		return "{}:{}".format(new_time, splited[1])
 
 # get the fajr prayer time
 fajr = get('FAJR')
@@ -45,4 +50,4 @@ formatted_isha = format_time(isha) if format == 12 else isha
 # current time
 dt = datetime.now()
 
-print("{} {} {} {} {}".format(fajr, zuhr, asr, magrib, isha))
+print("{} {} {} {} {}".format(formatted_fajr, formatted_zuhr, formatted_asr, formatted_magrib, formatted_isha))
