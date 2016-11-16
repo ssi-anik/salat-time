ada978de20a065a8564c6432208cb21f95024bb8
Sirajul <sirajul.islam.anik@gmail.com>
2016-11-16 17:18:48 +0600

    Moved .env.example/.env to config.py

---
+++ config.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+# time format in 12 hours (AM/PM) or 24 hours
+time_format = 12
+# Get notified before X minutes
+notify_before = 5
+# Enable tone - yes/no
+enable_tone = "yes"
+
+# Configure the time according to time format
+# Fajr salat time
+fajr="5:30AM"
+# Zuhr salat time
+zuhr="1:15PM"
+# Asr salat time
+asr="4:15PM"
+# Magrib salat time
+magrib="5:21PM"
+# Isha salat time
+isha="6:45PM"
